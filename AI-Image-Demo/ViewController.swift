49271b35da68a9bfbe381f92eed23834645bae03
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2023-03-02 17:14:37 +0100

    Initial Commit

---
+++ AI-Image-Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AI-Image-Demo
+//
+//  Created by Aitor Zubizarreta on 2023-03-02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
